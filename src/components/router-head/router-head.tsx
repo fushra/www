cfd0edde68ec80c746e30ef7fba95506f2ede279
TrickyPR <trickypr@icloud.com>
2022-10-29 23:14:21 +1100

    🚨 Fix the compiler

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -1,12 +1,12 @@
-import { component$ } from '@builder.io/qwik';
-import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
+import { component$ } from '@builder.io/qwik'
+import { useDocumentHead, useLocation } from '@builder.io/qwik-city'
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
  */
 export const RouterHead = component$(() => {
-  const head = useDocumentHead();
-  const loc = useLocation();
+  const head = useDocumentHead()
+  const loc = useLocation()
 
   return (
     <>
@@ -14,14 +14,14 @@ export const RouterHead = component$(() => {
 
       <link rel="canonical" href={loc.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      {/* <link rel="icon" type="image/svg+xml" href="/favicon.svg" /> */}
 
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link
         href="https://fonts.googleapis.com/css2?family=Poppins&amp;display=swap"
         rel="stylesheet"
-      />
+      /> */}
 
       {head.meta.map((m) => (
         <meta {...m} />
@@ -35,5 +35,5 @@ export const RouterHead = component$(() => {
         <style {...s.props} dangerouslySetInnerHTML={s.style} />
       ))}
     </>
-  );
-});
+  )
+})
